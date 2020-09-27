a95db81b9ea7ea2e38f29472a7154bdf16ee06a1
stefanoroque <stefanoroque98@gmail.com>
2020-09-27 21:55:40 +0200

    trying to fix static file issue

--- HonestClips_Website/wsgi.py
+++ HonestClips_Website/wsgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HonestClips_Website.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
+
+# application = get_wsgi_application()
